df9128fd18021507d257c23c5025d0b08de88856
hemanth-anamala-220 <hemanthanamala220@gmail.com>
2026-01-11 11:14:00 +0530

    Update App.jsx

--- cloud_project/src/App.jsx
+++ cloud_project/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { LogOut, Search } from 'lucide-react';
 import './App.css';
 
-const API_URL = 'https://cloud-based-file-storage-system-using.onrender.com';
+const API_URL = 'https://cloud-based-file-storage-system-using-y8hs.onrender.com';
 
 // Auth Context
 const AuthContext = React.createContext(null);
